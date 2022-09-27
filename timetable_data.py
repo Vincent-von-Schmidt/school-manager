fe370d460cde17db032683a40f476213614b49dc
Vincent v.S <vincent@Vincent-Desktop>
2022-09-27 21:55:47 +0200

    postgresql database test connection

---
+++ timetable_data.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+connection = psycopg2.connect(
+    host="192.168.178.23",
+    port="5400",
+    database="school",
+    user="postgres",
+    password="delta1"
+)
+
+cursor = connection.cursor()
+
+cursor.execute("""
+
+SELECT 
+	day.name AS day,
+	timetable.hour,
+	curs.name AS curs_id,
+	subject.name AS subject,
+	room.name as room,
+	FORMAT('%s, %s', teacher.last_name, teacher.first_name) AS teacher
+FROM
+	timetable JOIN curs ON timetable.curs = curs.id
+		JOIN day ON timetable.day = day.id
+		JOIN times ON timetable.hour = times.hour
+		JOIN subject ON curs.subject = subject.id
+		JOIN room ON curs.room = room.id
+		JOIN teacher ON curs.teacher = teacher.id
+WHERE timetable.semester = 1
+ORDER BY timetable.id ASC;
+
+""")
+
+print(cursor.fetchall())
