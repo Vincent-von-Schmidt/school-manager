4bed7b4981245f98ae9f6841041e1ecf228a2e2d
Vincent v.S <vincent@Vincent-Desktop>
2022-09-30 15:43:02 +0200

    debug function enables/disables GUI frame borders

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from window import Window
 from PyQt6.QtWidgets import QApplication
 import sys
+import data
 
 if __name__ == "__main__":
     app = QApplication([sys.argv])
     window = Window()
 
     with open("style/dark.css", "r") as file:
-        app.setStyleSheet(file.read())
+        style = file.read()
+    
+        if data.debug:
+            with open("style/debug.css", "r") as file:
+                style += file.read()
+
+        app.setStyleSheet(style)
 
     sys.exit(app.exec())
