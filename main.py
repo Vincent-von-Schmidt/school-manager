4df0c7878f669f8053d78ad6e9a14099503a92c6
Vincent v.S <vincent@Vincent-Desktop>
2022-09-30 16:44:23 +0200

    format

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     with open("style/dark.css", "r") as file:
         style = file.read()
     
-        if data.debug:
-            with open("style/debug.css", "r") as file:
-                style += file.read()
+    if data.debug:
+        with open("style/debug.css", "r") as file:
+            style += file.read()
 
-        app.setStyleSheet(style)
+    app.setStyleSheet(style)
 
     sys.exit(app.exec())
