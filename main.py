cde4c4a4f1c93ece6b19637eba497d62a6568bfc
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2022-10-24 22:21:20 +0200

    format

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from window import Window
+from src.app.window import Window
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtCore import Qt
 import sys
-import data
+import src.data as data
 
 if __name__ == "__main__":
     app = QApplication([sys.argv])
     window = Window()
     
-    with open("style/dark.css", "r") as file:
+    with open("src/app/style/dark.css", "r") as file:
         style = file.read()
     
     if data.debug:
-        with open("style/debug.css", "r") as file:
+        with open("src/app/style/debug.css", "r") as file:
             style += file.read()
 
     app.setStyleSheet(style)
