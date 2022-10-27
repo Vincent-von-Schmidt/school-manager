4385c0db77b218b41da9a9722b1cd40df7c8bca1
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2022-10-27 22:21:10 +0200

    create postgresql container

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get install docker.io -y
 docker build -t py_app .
-docker run -it --rm --name py_app py_app
+docker run -it -d --rm --name py_app py_app
+docker run -it -d --rm -p 5432:5432 --name postgresql postgres:latest
