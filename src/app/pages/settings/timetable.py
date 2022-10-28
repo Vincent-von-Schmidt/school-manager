2cfdb0aef4fa1ead7bd5532cf8919c3b48f42807
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2022-10-28 16:17:25 +0200

    working scroll area

--- src/app/pages/settings/timetable.py
+++ src/app/pages/settings/timetable.py
@@ -1,5 +1,11 @@
 from PyQt6.QtWidgets import (
-    QVBoxLayout, QLabel, QPushButton, QFrame, QScrollArea
+    QVBoxLayout, 
+    QLabel, 
+    QPushButton, 
+    QFrame, 
+    QScrollArea, 
+    QScrollBar,
+    QGroupBox
 )
 from src.widgets.div import Div
 from src.data import translate
@@ -12,15 +18,33 @@ class Timetable(QFrame):
     def __init__(self) -> None:
         super().__init__()
 
-        self.layout = QVBoxLayout(self)
+        scroll_area = QScrollArea(self)
+        scroll_area.setVerticalScrollBar(QScrollBar(self))
+        scroll_area.setWidgetResizable(True)
 
-        self.layout.addWidget(div1 := Div())
+        frame = QGroupBox(self)
+        self.frame_layout = QVBoxLayout(frame)
+
+        self.frame_layout.addWidget(div1 := Div())
         div1.addWidget(QLabel(translate("div 1")))
 
-        self.layout.addWidget(div2 := Div())
+        self.frame_layout.addWidget(div2 := Div())
         div2.addWidget(QLabel(translate("div 2")))
 
-        self.layout.addWidget(div3 := Div())
+        self.frame_layout.addWidget(div3 := Div())
         div3.addWidget(QLabel(translate("div 3")))
+
+        self.frame_layout.addWidget(div4 := Div())
+        div4.addWidget(QLabel(translate("div 4")))
+
+        self.frame_layout.addWidget(div5 := Div())
+        div5.addWidget(QLabel(translate("div 5")))
+
+        frame.setLayout(self.frame_layout)
+
+        scroll_area.setWidget(frame)
+        
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(scroll_area)
 
         self.setLayout(self.layout)
