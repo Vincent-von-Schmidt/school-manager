53886b5eacd0d16ba6215c736e598ea2c13fab4b
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2022-11-05 23:10:05 +0100

    added data types, implementation of new settings menu

--- src/app/pages/main_menu.py
+++ src/app/pages/main_menu.py
@@ -2,18 +2,21 @@ from PyQt6.QtWidgets import QFrame, QVBoxLayout
 from PyQt6.QtGui import QIcon
 from src.widgets.list_tabs import ListTabs
 from src.app.pages.timetable import Timetable
-from src.app.pages.settings.settings import Settings
+from src.app.pages.settingsNeo.settings import Settings as SettingsNeo
+from src.app.pages.dashboard import Dashboard
+from src.data import translate
 
 
 class MainMenu(QFrame):
     def __init__(self) -> None:
         super().__init__()
 
-        self.layout = QVBoxLayout(self)
+        self.layout: QVBoxLayout = QVBoxLayout(self)
 
-        self.layout.addWidget(content := ListTabs())
-        content.addWidget("Dashboard", QFrame(), QIcon("assets/home.ico"))
-        content.addWidget("Timetable", Timetable()) 
-        content.addWidget("settings", Settings(), QIcon("assets/settings.ico"))
+        content: ListTabs = ListTabs() 
+        content.addWidget(translate("Dashboard"), Dashboard(), QIcon("assets/home.ico"))
+        content.addWidget(translate("Timetable"), Timetable()) 
+        content.addWidget(translate("Settings"), SettingsNeo(), QIcon("assets/settings.ico"))
+        self.layout.addWidget(content)
 
         self.setLayout(self.layout)
