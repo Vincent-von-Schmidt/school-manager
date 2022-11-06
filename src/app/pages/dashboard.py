1e8bb7f86e85ee981ecb64b746fc7d5f6bc6cb40
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2022-11-06 14:18:23 +0100

    added data types

--- src/app/pages/dashboard.py
+++ src/app/pages/dashboard.py
@@ -15,38 +15,44 @@ class Dashboard(QFrame):
     def __init__(self) -> None:
         super().__init__()
 
-        self.layout = QVBoxLayout(self)
+        self.layout: QVBoxLayout = QVBoxLayout(self)
 
-        self.layout.addWidget(button := QPushButton("Div-style-test-button"))
+        button: QPushButton = QPushButton("Div-style-test-button")
         button.setObjectName("div")
-        button.setLayout(button_layout := QHBoxLayout(button))
-        button_layout.addWidget(icon1 := QLabel())
+        button_layout: QHBoxLayout = QHBoxLayout(button)
+        icon1: QLabel = QLabel()
         icon1.setObjectName("icon")
         icon1.setPixmap(QPixmap("assets/home.ico").scaled(
             40, 40,
             Qt.AspectRatioMode.KeepAspectRatio
         ))
-        button_layout.addWidget(QLabel("Div_style_test_button"))
-        button_layout.addWidget(icon2 := QLabel())
+        icon2: QLabel = QLabel()
         icon1.setObjectName("icon")
         icon2.setPixmap(QPixmap("assets/settings.ico").scaled(
             40, 40,
             Qt.AspectRatioMode.KeepAspectRatio
         ))
+        button_layout.addWidget(icon1)
+        button_layout.addWidget(QLabel("Div_style_test_button"))
+        button_layout.addWidget(icon2)
+        button.setLayout(button_layout)
+        self.layout.addWidget(button)
 
-        self.layout.addWidget(div_test := QFrame(self))
+        div_test: QFrame = QFrame(self)
         div_test.setObjectName("div")
-        div_test_layout = QHBoxLayout(div_test)
+        div_test_layout: QHBoxLayout = QHBoxLayout(div_test)
         div_test_layout.addWidget(QLabel("test1"))
         div_test_layout.addWidget(QCheckBox(div_test))
         div_test.setLayout(div_test_layout)
+        self.layout.addWidget(div_test)
 
-        self.layout.addWidget(div_button_test := QFrame(self))
+        div_button_test: QFrame = QFrame(self)
         div_button_test.setObjectName("div")
-        div_button_test_layout = QHBoxLayout(div_button_test)
+        div_button_test_layout: QHBoxLayout = QHBoxLayout(div_button_test)
         div_button_test_layout.addWidget(QPushButton("test1"))
         div_button_test_layout.addWidget(QPushButton("test2"))
         div_button_test.setLayout(div_button_test_layout)
+        self.layout.addWidget(div_button_test)
 
         """
         self.layout.addWidget(div_test := Div())
